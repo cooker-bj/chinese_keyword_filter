ab8a34e31ba3751a18ecdf0051c3d3fe9b028b03
kang cao <cooker_bj@hotmail.com>
2013-12-21 20:29:39 +0800

    version 1

--- lib/chinese_keyword_filter.rb
+++ lib/chinese_keyword_filter.rb
@@ -5,20 +5,31 @@ module ChineseKeywordFilter
     def initialize(options)
       @data=[]
       data_path="#{File.dirname(File.expand_path(__FILE__))}/#{ChineseKeywordFilter::NAME}/word.dat"
-      File.open(data_path,'r') do |f|
-        f.each_line do |line|
-          @data<<line.chomp
-        end
+       add_filter_data(data_path)
+
+      if defined? Rails
+        custom_path="#{Rails.root}/config/custom_filter.dat"
+        add_filter_data(custom_path)
       end
       super
+
     end
 
     def validate_each(object,attribute,value)
          @data.each do |key|
-          object.errors[attribute]<< (options[:message]||"请不要使用敏感词#{key}" )if value=~/#{key}/
+           object.errors[attribute]<< (options[:message]||"请不要使用敏感词" ) if value=~/#{key}/
          end
 
     end
+    private
+      def add_filter_data(path)
+        File.open(path,'r') do |f|
+          f.each_line do |line|
+            @data<<line.chop unless line.chop.empty?
+          end
+        end
+      end
+
 
   end
 end
